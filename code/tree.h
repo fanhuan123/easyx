c29e98554d25c1d240576fcc3607874b0b271ae9
fanhuan123 <151384747+fanhuan123@users.noreply.github.com>
2024-12-12 20:30:24 +0800

    Update tree.h

--- code/tree.h
+++ code/tree.h
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 #include<easyx.h>
+#include<graphics.h>
+#include<conio.h>
+#include<cmath>
+#define graph_width 2000
+#define graph_height 1000
+#define	bk_color WHITE
+#define fill_color RED
+#define line_color BLACK
+#define	text_color YELLOW
+#define	text_height 50
+#define text_width 20
 void imageput_alpha(IMAGE* img1,int x,int y);
 void init();
 void text_scan(char a[],int x,int y,int b);
 void gameexit_graph();
+void move(ExMessage* msg,int &x,int& y);
